56f6f88392e620ebd20ea68a1c74acf4ebc14f26
kimdonggu42 <kdk3280@undermilli.com>
2025-05-24 04:39:22 +0900

    style: step2 페이지 퍼블리싱

--- src/app/onboarding/step1/page.tsx
+++ src/app/onboarding/step1/page.tsx
@@ -1,19 +1,11 @@
-'use client';
-
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Step1() {
-  const router = useRouter();
-
-  const handleNext = () => {
-    router.push('/onboarding/step2');
-  };
-
   return (
     <div className='flex h-screen flex-col items-center justify-center bg-[#F7F4F2] px-6'>
       {/* 상단 텍스트 */}
-      <div className='font-pretendard mb-24 text-left text-2xl font-bold'>
+      <div className='font-pretendard mb-24 self-start text-left text-2xl font-bold'>
         <p className='text-gray-800'>마음에 남아 있는,</p>
         <p className='text-gray-800'>동네의 순간을 기록해보세요.</p>
       </div>
@@ -24,8 +16,8 @@ export default function Step1() {
         <Image
           src='/step1.png'
           alt='마음에 남은 순간을 기록하는 캐릭터'
-          width={300}
-          height={300}
+          width={250}
+          height={250}
           className='object-contain'
         />
 
@@ -43,12 +35,12 @@ export default function Step1() {
 
       {/* 하단 버튼 */}
       <div className='mt-30 w-full max-w-sm'>
-        <button
-          onClick={handleNext}
-          className='w-full rounded-full bg-black px-6 py-4 text-sm font-medium text-white'
+        <Link
+          href='/onboarding/step2'
+          className='block w-full rounded-full bg-black px-6 py-4 text-center text-sm font-medium text-white'
         >
           다음
-        </button>
+        </Link>
       </div>
     </div>
   );
